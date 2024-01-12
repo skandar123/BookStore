533062561c2bb10029a2355bcb165c6e138e641d
skandar123 <35923180+skandar123@users.noreply.github.com>
2024-01-12 13:19:36 +0530

    Update menu.php

--- BuyBooks/partials-front/menu.php
+++ BuyBooks/partials-front/menu.php
@@ -11,8 +11,9 @@
 <title>Online BookStore</title>
 
 <!-- Link to CSS file -->
-<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../CSS/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+<link rel="icon" type="image/x-icon" href="../images/bookstore_logo.png">
 </head>
 <body>
 <?php $activePage = basename($_SERVER['PHP_SELF'], ".php"); ?>
@@ -29,7 +30,22 @@
 					<a class="<?= ($activePage == 'index') ? 'active':''; ?>" href="index.php">Home</a>
 				</li>
 				<li>
-					<a class="<?= ($activePage == 'categories') ? 'active':''; ?>" href="categories.php">Categories</a>
+					<a class="<?= ($activePage == 'about') ? 'active':''; ?>" href="about.php">About Us</a>
+				</li>
+				<li>
+					<a class="<?= ($activePage == 'gallery') ? 'active':''; ?>" href="gallery.php">Gallery</a>
+				</li>
+				<li class="dropdown">
+					<a class="dropbtn" href="categories.php">Categories</a>
+					<div class="dropdown-content">
+      					<a href="category-books.php?category_id=13">GATE</a>
+      					<a href="category-books.php?category_id=14">GRE</a>
+      					<a href="category-books.php?category_id=15">TOEFL</a>
+      					<a href="category-books.php?category_id=16">IELTS</a>
+      					<a href="category-books.php?category_id=17">IITJEE</a>
+      					<a href="category-books.php?category_id=18">JEE</a>
+      					<a href="category-books.php?category_id=19">MHT-CET</a>
+    				</div>
 				</li>
 				<li>
 					<a class="<?= ($activePage == 'books') ? 'active':''; ?>" href="books.php">Books</a>
@@ -37,7 +53,7 @@
 			    <li>
 					<a class="<?= ($activePage == 'contact') ? 'active':''; ?>" href="contact.php">Contact</a>
 				</li>
-				<li>
+				<li style="float:right">
 					<a class="<?= ($activePage == 'logout') ? 'active':''; ?>" href="logout.php">Logout</a>
 				</li>
 		</ul>
