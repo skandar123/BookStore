ea389037b0a12aef376a5357ed8c829d02b37ecf
skandar123 <35923180+skandar123@users.noreply.github.com>
2024-01-12 13:17:04 +0530

    Update index.php

--- BuyBooks/partials-front/index.php
+++ BuyBooks/partials-front/index.php
@@ -22,12 +22,15 @@ if(isset($_SESSION['order']))
 
 <!-- Start of categories Section -->
 <section class="categories">
-	<div class="container">
-	    <div>
-		<h2 class="text-center text-white">Explore Books</h2>
-		<p class="text-center text-white">Get books of different competitive exams at a cheaper price. Click on the different competitive exams below to view and order all the books available for the selected exam.</p>
-		<br>
-		</div>
+	
+	    <div class="container gradient-yellow-container">
+		<h2 class="text-center">Explore Books</h2>
+		<p class="text-center">
+		Get books of different competitive exams at a cheaper price. 
+		Click on the different competitive exams below to view and order all the books available for the selected exam.
+		</p>
+		
+		
 		<div class="grid-container">
 		<?php 
 		//To display data from database
@@ -48,13 +51,13 @@ if(isset($_SESSION['order']))
 		        $title=$row['title'];
 		        $image_name=$row['image_name'];
 		        ?>
-		        
-        		<a href="<?php echo SITEURL; ?>partials-front/category-books.php?category_id=<?php echo $id; ?>">
-        		<div class="grid-item">
-        			 <h3><?php echo $title; ?></h3>  
-        		</div>
-        		</a>
+		        <div class="category-item">
+        		<a class="category-text" href="<?php echo SITEURL; ?>partials-front/category-books.php?category_id=<?php echo $id; ?>">
         		
+        			 <?php echo $title; ?>
+        		
+        		</a>
+        		</div>
         		<?php 
 		    }
 		}
@@ -65,8 +68,9 @@ if(isset($_SESSION['order']))
 		}
 		?>
 		
-		<div class="clearfix"></div>
+		<!--  <div class="clearfix"></div> -->
 		</div>
+		
 	</div>
 </section>
 <!-- End of categories Section -->
@@ -75,7 +79,7 @@ if(isset($_SESSION['order']))
 <section class="book-menu">
 	<div class="container">
 	
-		<h2 class="text-center">Book Menu</h2>
+		<h2 class="text-center">Best Selling Books</h2>
 		<div class="grid-container">
 		<?php 
 		//Getting books from databse that are active and featured
@@ -150,5 +154,87 @@ if(isset($_SESSION['order']))
 	</div>
 </section>
 <!-- End of Book Menu Section -->
+
+<!-- Start of Reviews Section -->
+<section class="reviews">
+<div class="container gradient-green-container">
+	<br><br>
+		<h2 class="text-center">Reviews</h2>
+	<div class="medium-container">	
+		<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="../images/students/abhimanyu_singh.jpeg" style="width:100%">  
+  <div class="text">
+  <b>Abhimanyu Singh</b><br>
+  "I got the perfect set of books and notes to crack the IITJEE exam. 
+  They also provided free subscription to different youtube channels to prepare for IITJEE exam."
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="../images/students/sujata_mishra.jpeg" style="width:100%">
+  <div class="text">
+  <b>Sujata Mishra</b><br>
+  "They provide notes of toppers which has saved a lot of my time.
+  Their video lectures helped me with quick revision."
+  </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="../images/students/pranav_gupta.jpeg" style="width:100%">
+  <div class="text">
+  <b>Pranav Gupta</b><br>
+  "They not only provide books but also provide access to a community of 
+  dedicated students and excellent professors which is a great option for students who cannot afford coaching centers."
+  </div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+	</div></div>
+</section>
+<!-- End of Reviews Section -->
 
 <?php include('footer.php'); ?>
